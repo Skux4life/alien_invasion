7fd3c1aebeeea6edf73214b8ff9b606b8e888e8e
Skux4life <mattellis1234@gmail.com>
2023-04-05 16:48:56 +1000

    add scoreboard

--- settings.py
+++ settings.py
@@ -11,19 +11,39 @@ class Settings:
         self.bg_colour = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 3.0
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 5.0
         self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_colour = (60, 60, 60)
         self.bullets_allowed = 5
 
         # Alien settings
+        self.fleet_drop_speed = 10
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+        # How quickly the alien point value increases
+        self.score_scale = 1.5
+        
+        self.init_dynamic_settings()
+
+    def init_dynamic_settings(self):
+        """Initialize settings that change throughout the game"""
+        self.ship_speed = 3.0
+        self.bullet_speed = 5.0
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 50
         # fleet_direction of 1 means right and -1 means left
         self.fleet_direction = 1
+        # Scoring settings
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase speed settings and alien points value"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+
         
