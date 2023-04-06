1a1633afd8eca4257d5be2b9d29bd3d4acb305c5
Matthew Ellis <mattellis1234@gmail.com>
2023-04-06 15:20:13 +1000

    added methods to save and retrieve high score from a txt file (#3)

--- game_stats.py
+++ game_stats.py
@@ -1,13 +1,24 @@
+import os
+
+import constants
+
 class GameStats:
     """Track statistics for Alien Invasion"""
 
     def __init__(self, ai_game) -> None:
         self.settings = ai_game.settings
         # High score shouldn't be reset
-        self.high_score = 0
+        self.set_high_score()
         self.reset_stats()
 
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
+
+    def set_high_score(self):
+        """Retrieves the high score from a file if it exists"""
+        if os.path.isfile(constants.HIGH_SCORE_FILE):
+            self.high_score = int(open(constants.HIGH_SCORE_FILE).read())
+        else:
+            self.high_score = 0
